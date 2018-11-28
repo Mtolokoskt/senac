0a72a93b3044527083364d6ed758f8a5681ded4c
SENACEDU\bruno.lbrfaria <brunoluizbenini@hotmail.com>
2018-11-27 22:04:02 -0200

    clipboard
    Feito

--- senac_2016/js/script.js
+++ senac_2016/js/script.js
@@ -1,3 +1,9 @@
+
+$( document ).ready(function() {
+    new ClipboardJS('.btn');
+});
+
+
 function monstrar() {
     var curso = document.getElementById("curso").value;
     var descricao = document.getElementById("descricao").value;
@@ -18,42 +24,41 @@ function monstrar() {
     document.getElementById("m_id_docente").innerHTML = id_docente;
     document.getElementById("m_nome_docente").innerHTML = nome_docente;
     document.getElementById("m_justificativa").innerHTML = justificativa;
+
 };
 
-    function mostrar_radio(){
+function mostrar_radio() {
 
-        var parte = document.getElementsByName("radio_parte");
+    var parte = document.getElementsByName("radio_parte");
 
-        for (var i = 0, length = parte.length; i < length; i++)
-        {
-            if (parte[i].checked)
-            {
-                var m_parte = parte[i].value; 
-                document.getElementById("m_parte").innerHTML = m_parte;
-                break;
-            }
+    for (var i = 0, length = parte.length; i < length; i++) {
+        if (parte[i].checked) {
+            var m_parte = parte[i].value;
+            document.getElementById("m_parte").innerHTML = m_parte;
+            break;
         }
-
     }
 
-    function mostrar_motivo(){
+}
 
-        var motivo = document.getElementsByName("radio_mot");
+function mostrar_motivo() {
 
-        for (var i = 0, length = motivo.length; i < length; i++)
-        {
-            if (motivo[i].checked)
-            {
-                var m_parte = motivo[i].value; 
-                document.getElementById("m_motivo").innerHTML = m_parte;
-                break;
-            }
+    var motivo = document.getElementsByName("radio_mot");
+
+    for (var i = 0, length = motivo.length; i < length; i++) {
+        if (motivo[i].checked) {
+            var m_parte = motivo[i].value;
+            document.getElementById("m_motivo").innerHTML = m_parte;
+            break;
         }
-
     }
 
-    function copiar() {
-        var copyText = document.getElementById("mostrar");
-        copyText.select();
-        document.execCommand("copy");        
-      }
+}
+
+function clipboard(){
+    setTimeout(function(){
+        if (window.getSelection) {window.getSelection().removeAllRanges();}
+    else if (document.selection) {document.selection.empty();}
+    },50);
+    
+}
